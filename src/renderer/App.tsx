85153639fde3eecbc8462ff9134734a13de54d67
Supun D <139330393+Supun-SD@users.noreply.github.com>
2025-08-13 21:45:43 +0530

    Update App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Calculate from './pages/Calculate/Calculate';
 import Accounts from './pages/Accounts/Accounts';
