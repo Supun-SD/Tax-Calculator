0e6d2172ef8cb9dc5c828fe89c9570e9e0977299
Supun D <139330393+Supun-SD@users.noreply.github.com>
2025-09-05 19:40:24 +0530

    update configs remove

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,5 +1,5 @@
 import { app, BrowserWindow, dialog } from 'electron';
-import { autoUpdater } from 'electron-updater';
+// import { autoUpdater } from 'electron-updater';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
 import { HIDE_MENUBAR } from '../renderer/config/api';
@@ -34,55 +34,56 @@ const createWindow = () => {
   }
 };
 
-const createProgressWindow = () => {
-  progressWindow = new BrowserWindow({
-    width: 400,
-    height: 200,
-    resizable: false,
-    minimizable: true,
-    maximizable: false,
-    closable: true,
-    alwaysOnTop: true,
-    modal: true,
-    parent: mainWindow || undefined,
-    autoHideMenuBar: true,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-    },
-  });
+// const createProgressWindow = () => {
+//   progressWindow = new BrowserWindow({
+//     width: 400,
+//     height: 200,
+//     resizable: false,
+//     minimizable: true,
+//     maximizable: false,
+//     closable: true,
+//     alwaysOnTop: true,
+//     modal: true,
+//     parent: mainWindow || undefined,
+//     autoHideMenuBar: true,
+//     webPreferences: {
+//       nodeIntegration: false,
+//       contextIsolation: true,
+//     },
+//   });
 
-  const progressHtmlPath = path.join(__dirname, '../../public/progress-window.html');
-  progressWindow.loadFile(progressHtmlPath);
+//   const progressHtmlPath = path.join(__dirname, '../../public/progress-window.html');
+//   progressWindow.loadFile(progressHtmlPath);
   
-  if (mainWindow) {
-    mainWindow.setEnabled(false);
-  }
+//   if (mainWindow) {
+//     mainWindow.setEnabled(false);
+//   }
 
-  progressWindow.on('closed', () => {
-    progressWindow = null;
-    if (mainWindow) {
-      mainWindow.close();
-      mainWindow = null;
-    }
-  });
-};
+//   progressWindow.on('closed', () => {
+//     progressWindow = null;
+//     if (mainWindow) {
+//       mainWindow.close();
+//       mainWindow = null;
+//     }
+//   });
+// };
 
-const closeProgressWindow = () => {
-  if (progressWindow) {
-    progressWindow.close();
-    progressWindow = null;
-  }
-  if (mainWindow) {
-    mainWindow.setEnabled(true);
-  }
-};
+// const closeProgressWindow = () => {
+//   if (progressWindow) {
+//     progressWindow.close();
+//     progressWindow = null;
+//   }
+//   if (mainWindow) {
+//     mainWindow.setEnabled(true);
+//   }
+// };
 
 app.on('ready', () => {
   createWindow();
-  autoUpdater.autoDownload = false; 
-  autoUpdater.autoInstallOnAppQuit = true;
-  autoUpdater.checkForUpdates();
+  mainWindow.maximize();
+  // autoUpdater.autoDownload = false; 
+  // autoUpdater.autoInstallOnAppQuit = true;
+  // autoUpdater.checkForUpdates();
 });
 
 app.on('window-all-closed', () => {
@@ -97,93 +98,93 @@ app.on('activate', () => {
   }
 });
 
-autoUpdater.on('update-available', async (info) => {
-  if (mainWindow) {
-    mainWindow.setEnabled(false);
-  }
+// autoUpdater.on('update-available', async (info) => {
+//   if (mainWindow) {
+//     mainWindow.setEnabled(false);
+//   }
 
-  const result = await dialog.showMessageBox(mainWindow!, {
-    type: 'question',
-    buttons: ['Download', 'Close App'],
-    defaultId: 0,
-    cancelId: 1,
-    title: 'Update available',
-    message: `A new version (${info.version}) is available.\n\nSize: ${(info.files[0].size / (1024*1024)).toFixed(2)} MB\n\nDo you want to download it now?`,
-    noLink: true,
-    normalizeAccessKeys: true,
-  });
+//   const result = await dialog.showMessageBox(mainWindow!, {
+//     type: 'question',
+//     buttons: ['Download', 'Close App'],
+//     defaultId: 0,
+//     cancelId: 1,
+//     title: 'Update available',
+//     message: `A new version (${info.version}) is available.\n\nSize: ${(info.files[0].size / (1024*1024)).toFixed(2)} MB\n\nDo you want to download it now?`,
+//     noLink: true,
+//     normalizeAccessKeys: true,
+//   });
 
-  if (mainWindow) {
-    mainWindow.setEnabled(true);
-    mainWindow.focus();
-  }
+//   if (mainWindow) {
+//     mainWindow.setEnabled(true);
+//     mainWindow.focus();
+//   }
 
-  if (result.response === 0) {
-    createProgressWindow();
-    autoUpdater.downloadUpdate();
-  } else {
-    app.quit();
-  }
-});
+//   if (result.response === 0) {
+//     createProgressWindow();
+//     autoUpdater.downloadUpdate();
+//   } else {
+//     app.quit();
+//   }
+// });
 
-autoUpdater.on('download-progress', (progress) => {
-  if (mainWindow) {
-    mainWindow.setProgressBar(progress.percent / 100); 
-  }
+// autoUpdater.on('download-progress', (progress) => {
+//   if (mainWindow) {
+//     mainWindow.setProgressBar(progress.percent / 100); 
+//   }
   
-  if (progressWindow) {
-    progressWindow.webContents.executeJavaScript(`
-      if (window.updateProgress) {
-        window.updateProgress(${progress.percent}, ${progress.bytesPerSecond}, ${progress.transferred}, ${progress.total});
-      }
-    `);
-  }
-});
+//   if (progressWindow) {
+//     progressWindow.webContents.executeJavaScript(`
+//       if (window.updateProgress) {
+//         window.updateProgress(${progress.percent}, ${progress.bytesPerSecond}, ${progress.transferred}, ${progress.total});
+//       }
+//     `);
+//   }
+// });
 
-autoUpdater.on('update-downloaded', async () => {
+// autoUpdater.on('update-downloaded', async () => {
   
-  closeProgressWindow();
-  if (mainWindow) mainWindow.setProgressBar(-1); 
+//   closeProgressWindow();
+//   if (mainWindow) mainWindow.setProgressBar(-1); 
 
-  if (mainWindow) {
-    mainWindow.setEnabled(false);
-  }
+//   if (mainWindow) {
+//     mainWindow.setEnabled(false);
+//   }
 
-  const result = await dialog.showMessageBox(mainWindow!, {
-    type: 'question',
-    buttons: ['Install and Restart'],
-    defaultId: 0,
-    cancelId: 1,
-    title: 'Update ready',
-    message: 'The update has been downloaded successfully. Do you want to install it now?',
-    noLink: true,
-    normalizeAccessKeys: true,
-  });
+//   const result = await dialog.showMessageBox(mainWindow!, {
+//     type: 'question',
+//     buttons: ['Install and Restart'],
+//     defaultId: 0,
+//     cancelId: 1,
+//     title: 'Update ready',
+//     message: 'The update has been downloaded successfully. Do you want to install it now?',
+//     noLink: true,
+//     normalizeAccessKeys: true,
+//   });
 
-  if (mainWindow) {
-    mainWindow.setEnabled(true);
-    mainWindow.focus();
-  }
+//   if (mainWindow) {
+//     mainWindow.setEnabled(true);
+//     mainWindow.focus();
+//   }
 
-  if (result.response === 0) {
-    autoUpdater.quitAndInstall();
-  }
-});
+//   if (result.response === 0) {
+//     autoUpdater.quitAndInstall();
+//   }
+// });
 
 
-autoUpdater.on('error', (error) => {
-  closeProgressWindow();
+// autoUpdater.on('error', (error) => {
+//   closeProgressWindow();
   
-  dialog.showErrorBox('Update Error', 
-    `Failed to download update: ${error.message}\n\nPlease try again later.`
-  );
+//   dialog.showErrorBox('Update Error', 
+//     `Failed to download update: ${error.message}\n\nPlease try again later.`
+//   );
 
-  if (mainWindow) {
-    mainWindow.setEnabled(true);
-  }
-});
+//   if (mainWindow) {
+//     mainWindow.setEnabled(true);
+//   }
+// });
 
 
-autoUpdater.on('update-cancelled', () => {
-  closeProgressWindow();
-});
+// autoUpdater.on('update-cancelled', () => {
+//   closeProgressWindow();
+// });
